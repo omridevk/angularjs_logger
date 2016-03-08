90b22f741aa1b62ef9a0fd8c6a31960912200acf
omri <omrikatz12@gmail.com>
2016-03-09 00:15:33 +0200

    commit

--- src/logger/loggerProvider.js
+++ src/logger/loggerProvider.js
@@ -3,7 +3,7 @@
  */
 
 (function() {
-    'use strict';
+    //'use strict';
 
     angular
         .module('logger')
@@ -13,15 +13,23 @@
     function logger() {
         var debug = true,
             self = this,
-            styles = 'background: #222; color: #bada55';
+            styles = 'background: #222; color: #8BC34A;',
+            method_name;
 
 
-        this.setStyle = function(style) {
-            styles = style;
+        /**
+         * @name loggerProvider#setStyle
+         * @description
+         * @param {String} string contain css rules.
+         * @param {Boolean} Override default style.
+         * @returns {*} current value if used as getter or itself (chaining) if used as setter
+         */
+
+        this.setStyle = function(style, override) {
+            styles = (override) ? style : styles + style;
             return this;
         };
         /**
-         * @ngdoc method
          * @name $logProvider#debugEnabled
          * @description
          * @param {boolean=} flag enable or disable debug level messages
@@ -36,29 +44,42 @@
             }
         };
 
-        this.$get = ['$window', function($window) {
+        this.$get = ['$window', '$location', function($window, $location) {
             return {
                 /**
-                 * @ngdoc method
-                 * @name $log#log
+                 * @name logger#log
                  *
                  * @description
                  * Write a log message
                  */
-                log: consoleLog('log'),
+                log: (function() {
+                    var fn = consoleLog('log');
+
+                    return function() {
+                        if (_isEnabled()) {
+                            fn.apply(self, arguments);
+                        }
+                    };
+                }()),
 
                 /**
-                 * @ngdoc method
-                 * @name $log#info
+                 * @name logger#info
                  *
                  * @description
                  * Write an information message
                  */
-                info: consoleLog('info'),
+                info: (function() {
+                    var fn = consoleLog('info');
+
+                    return function() {
+                        if (_isEnabled()) {
+                            fn.apply(self, arguments);
+                        }
+                    };
+                }()),
 
                 /**
-                 * @ngdoc method
-                 * @name $log#warn
+                 * @name logger#warn
                  *
                  * @description
                  * Write a warning message
@@ -66,8 +87,7 @@
                 warn: consoleLog('warn'),
 
                 /**
-                 * @ngdoc method
-                 * @name $log#error
+                 * @name logger#error
                  *
                  * @description
                  * Write an error message
@@ -75,8 +95,7 @@
                 error: consoleLog('error'),
 
                 /**
-                 * @ngdoc method
-                 * @name $log#debug
+                 * @name logger#debug
                  *
                  * @description
                  * Write a debug message
@@ -85,15 +104,43 @@
                     var fn = consoleLog('debug');
 
                     return function() {
-                        if (debug) {
+                        if (_isEnabled()) {
                             fn.apply(self, arguments);
                         }
                     };
                 }())
             };
 
+            /**
+             * @name logger#debug
+             *
+             * @description
+             * Write a debug message
+             */
+
+            function _isEnabled() {
+                var queryParam = $location.search() || {};
+                return ('debugEnabled' in queryParam) || debug;
+                //if ('debugEnabled' in queryParam) {
+                //    return true;
+                //}
+            }
+
+
+
+            function _addPrefix(arguments) {
+                if (method_name.length > 0) {
+                    arguments[0] = method_name + ': ' + arguments[0];
+                }
+                arguments[0] = "%c " + arguments[0];
+                return arguments;
+            }
+
+
 
             function consoleLog(type) {
+
+
                 var console = $window.console || {},
                     logFn = console[type] || console.log || noop,
                     hasApply = false;
@@ -107,11 +154,13 @@
                 if (hasApply) {
                     return function() {
                         var args = [];
+                        method_name = arguments.callee.caller.name || '';
                         angular.forEach(arguments, function(arg) {
                             args.push(arg);
                         });
-                        args[0] = "%c " + args[0] + " %s"
+
                         args.splice(1,0, styles);
+                        args = _addPrefix(args);
                         return logFn.apply(console, args);
                     };
                 }
